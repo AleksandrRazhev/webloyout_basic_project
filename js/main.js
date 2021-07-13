c3c23c985ac22c86913c826c9c01f29feab1efbd
RazhevAleksandr <alexandrrazh@gmail.com>
2021-07-13 13:44:11 +0400

    js correct

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('.header-bottom__nav-item').forEach(function(openList) {
     openList.addEventListener('click', function() {
       if (this.classList.contains('dropdown-list-active')) {
-        this.classList.remove('dropdown-list-opacity')
-        this.classList.remove('dropdown-list-active')
+        this.classList.add('dropdown-list-opacity')
+        setTimeout(this.classList.remove('dropdown-list-active'), 5000)
+        setTimeout(this.classList.remove('dropdown-list-opacity'), 5000)
       } else {
           document.querySelectorAll('.header-bottom__nav-item').forEach(function(classRemove) {
             classRemove.classList.remove('dropdown-list-opacity')
             classRemove.classList.remove('dropdown-list-active')
         })
         this.classList.add('dropdown-list-active')
-        this.classList.add('dropdown-list-opacity')
       }
     })
   })
