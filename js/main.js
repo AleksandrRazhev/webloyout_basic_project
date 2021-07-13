8c27f44a4f55e22cc17d008ecd9f463cb4c2cc8c
RazhevAleksandr <alexandrrazh@gmail.com>
2021-07-13 16:27:32 +0400

    transition dropdown add, there is a problem with transition .dropdown-list-active

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('.header-bottom__nav-item').forEach(function(openList) {
     openList.addEventListener('click', function() {
-      if (this.classList.contains('dropdown-list-active')) {
-        this.classList.add('dropdown-list-opacity')
-        setTimeout(this.classList.remove('dropdown-list-active'), 5000)
-        setTimeout(this.classList.remove('dropdown-list-opacity'), 5000)
+
+      const elem = this
+
+      if (elem.classList.contains('dropdown-list-active')) {
+        this.classList.toggle('dropdown-list-opacity')
+        setTimeout(function() {
+          elem.classList.remove('dropdown-list-active')
+        }, 3000)
       } else {
           document.querySelectorAll('.header-bottom__nav-item').forEach(function(classRemove) {
             classRemove.classList.remove('dropdown-list-opacity')
             classRemove.classList.remove('dropdown-list-active')
         })
         this.classList.add('dropdown-list-active')
+        setTimeout(function() {
+          elem.classList.add('dropdown-list-opacity')
+        })
       }
     })
   })
