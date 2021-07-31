ac59aee50827d0ed17ada0d24dc6ad7435dc8463
RazhevAleksandr <alexc23@mail.ru>
2021-07-31 16:15:12 +0300

    problem: remove modal window object

--- js/gallery_modal.js
+++ js/gallery_modal.js
@@ -2,18 +2,32 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.gallery__swiper-slide').forEach(function(galleryPicture) {
         galleryPicture.addEventListener('click', function(event) {
             event.preventDefault()
-            openGalleryPicture(event.currentTarget)
+            openModalWindow(event.currentTarget)
+            return imgModalWindow = event.currentTarget
         }, false)
+        document.querySelector('.gallery__mod-win-close').addEventListener('click', function(){
+            closeModalWindow(imgModalWindow)
+        })
     })
   })
   
-  function openGalleryPicture(elem) {
+  function openModalWindow(elem) {
     document.querySelectorAll('.gallery__swiper-slide').forEach(function(classRemove) {
         classRemove.classList.remove('open__swiper-slide')
     })
+    elem.style.marginTop=0
+    elem.style.marginBottom=0
+    elem.style.marginRight=0
+    elem.style.marginLeft=0
     elem.classList.add('open__swiper-slide')
     document.body.classList.toggle('body__background')
-    document.querySelector('.gallery__inner').classList.toggle('gallery__inner-on')
-    let innerChild = document.querySelector('.gallery__inner-img').querySelector('.gallery__swiper-slide')
-    parent.querySelector('.gallery__inner-img').replaceChild(innerChild, elem)
+    document.querySelector('.gallery__mod-win').classList.toggle('gallery__mod-win-on')
+
+    document.querySelector('.gallery__mod-win-img').appendChild(elem)
+  }
+
+  function closeModalWindow(elem) {
+    document.querySelector('.gallery__mod-win-img').removeChild(elem)
+    document.body.classList.remove('body__background')
+    document.querySelector('.gallery__mod-win').classList.remove('gallery__mod-win-on')
   }
