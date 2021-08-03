bece8670c6fa058470d5682d6f6bd567661eb8ca
RazhevAleksandr <alexandrrazh@gmail.com>
2021-08-03 15:58:38 +0300

    section gallery with modal window finished

--- js/gallery_modal.js
+++ js/gallery_modal.js
@@ -2,32 +2,39 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.gallery__swiper-slide').forEach(function(galleryPicture) {
         galleryPicture.addEventListener('click', function(event) {
             event.preventDefault()
-            openModalWindow(event.currentTarget)
-            return imgModalWindow = event.currentTarget
+            const galleryNumberElement = event.currentTarget.dataset.galleryImg
+            const eventCopy = event.currentTarget.cloneNode(true)
+            const descriptonCopy = document.querySelector(`[data-gallery-txt="${galleryNumberElement}"]`).cloneNode(true)
+            openModalWindow(eventCopy, descriptonCopy)
+            return imgModalWindow = eventCopy, txtModalWindow = descriptonCopy
         }, false)
         document.querySelector('.gallery__mod-win-close').addEventListener('click', function(){
-            closeModalWindow(imgModalWindow)
+            closeModalWindow(imgModalWindow, txtModalWindow)
         })
     })
   })
   
-  function openModalWindow(elem) {
+  function openModalWindow(elemImg, elemTxt) {
     document.querySelectorAll('.gallery__swiper-slide').forEach(function(classRemove) {
         classRemove.classList.remove('open__swiper-slide')
     })
-    elem.style.marginTop=0
-    elem.style.marginBottom=0
-    elem.style.marginRight=0
-    elem.style.marginLeft=0
-    elem.classList.add('open__swiper-slide')
+    elemImg.style.marginTop=0
+    elemImg.style.marginBottom=0
+    elemImg.style.marginRight=0
+    elemImg.style.marginLeft=0
+    elemImg.style.width='auto'
+    elemImg.classList.add('open__swiper-slide')
     document.body.classList.toggle('body__background')
     document.querySelector('.gallery__mod-win').classList.toggle('gallery__mod-win-on')
-
-    document.querySelector('.gallery__mod-win-img').appendChild(elem)
+    document.querySelector('.gallery__swiper-text').classList.toggle('.gallery__swiper-text-on')
+    document.querySelector('.gallery__mod-win-left').appendChild(elemImg)
+    document.querySelector('.gallery__mod-win-right').appendChild(elemTxt)
   }
 
-  function closeModalWindow(elem) {
-    document.querySelector('.gallery__mod-win-img').removeChild(elem)
+  function closeModalWindow(elemImg, elemTxt) {
+    document.querySelector('.gallery__mod-win-left').removeChild(elemImg)
+    document.querySelector('.gallery__mod-win-right').removeChild(elemTxt)
     document.body.classList.remove('body__background')
     document.querySelector('.gallery__mod-win').classList.remove('gallery__mod-win-on')
+    document.querySelector('.gallery__swiper-text').classList.remove('.gallery__swiper-text-on')
   }
