1bf6a1d49c7cedad509e70526638a818ae7db440
RazhevAleksandr <alexc23@mail.ru>
2021-08-11 22:07:52 +0300

    add catalog menu and accordion

---
+++ js/catalog_accordion.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.catalog__menu-description').forEach(function(accordionClick) {
+        accordionClick.addEventListener('click', function(event) {
+
+            const accordionOpenElem = event.currentTarget
+            const accordionOpenElemParent = event.currentTarget.parentNode
+
+            if (accordionOpenElem.classList.contains('accordion-open')) {
+                if (accordionOpenElemParent.querySelector('.catalog__menu-item-block')) {
+                    setTimeout(function() {
+                        document.querySelector('.catalog__menu-item-block-on').classList.remove('catalog__menu-item-block-on')
+                    }, 300)
+                } else if (accordionOpenElemParent.querySelector('.catalog__menu-empty-block')) {
+                    setTimeout(function() {
+                        document.querySelector('.catalog__menu-empty-block-on').classList.remove('catalog__menu-empty-block-on')
+                    }, 300)
+                }
+                accordionOpenElem.classList.remove('accordion-open')
+                accordionOpenElemParent.classList.remove('accordion-open-parent')    
+            } else {
+                removeAccordionClasses()
+                removeListClasses()
+                if (accordionOpenElemParent.querySelector('.catalog__menu-item-block')) {
+                    accordionOpenElemParent.querySelector('.catalog__menu-item-block').classList.add('catalog__menu-item-block-on')
+                    setTimeout(addAccorionClasses, 0, accordionOpenElem, accordionOpenElemParent)
+                } else if (accordionOpenElemParent.querySelector('.catalog__menu-empty-block')) {
+                    addEmptyBlockClasses(accordionOpenElem, accordionOpenElemParent)
+                } else {
+                    //начало, убрать else
+
+                    let artistEmptyInner = document.createElement('div')
+                    artistEmptyInner.classList.add('catalog__menu-empty-block')
+                    artistEmptyInner.innerHTML = `
+                    <span class="catalog__empty-block-img"></span>
+                    <div class="catalog__empty-block-right">
+                    <strong class="catalog__empty-block-strong">Здесь пока пусто</strong>
+                    <p class="catalog__empty-block-text">А в галерее вы всегда можете найти что-то интересное для себя</p>
+                    <a class="catalog__empty-block-link" href="#gallery">В галерею</a>
+                    </div>
+                    `
+                    accordionOpenElemParent.append(artistEmptyInner)
+
+                    addEmptyBlockClasses(accordionOpenElem, accordionOpenElemParent)
+                }
+                //конец
+            }
+        })
+    })
+})
+
+function addAccorionClasses(accordionOpenElem, accordionOpenElemParent) {
+    accordionOpenElem.classList.toggle('accordion-open')
+    accordionOpenElemParent.classList.toggle('accordion-open-parent')
+}
+
+function removeAccordionClasses() {
+    document.querySelectorAll('.catalog__menu-description').forEach(function(classRemove1) {
+        classRemove1.classList.remove('accordion-open')
+    })
+    document.querySelectorAll('.catalog__menu-item').forEach(function(classRemove2) {
+        classRemove2.classList.remove('accordion-open-parent')
+    })
+}
+
+function removeEmptyBlockClasses() {
+    setTimeout(function() {
+        document.querySelectorAll('.catalog__menu-empty-block-on').forEach(function(classRemove3) {
+            classRemove3.classList.remove('.catalog__menu-empty-block-on')
+        })
+    }, 300)
+}
+
+function removeListClasses() {
+    setTimeout(function() {
+        document.querySelectorAll('.catalog__menu-item-block').forEach(function(classRemove3) {
+            classRemove3.classList.remove('.catalog__menu-item-block-on')
+        })
+    }, 300)
+}
+
+function addEmptyBlockClasses(accordionOpenElem, accordionOpenElemParent) {
+    accordionOpenElemParent.querySelector('.catalog__menu-empty-block').classList.add('catalog__menu-empty-block-on')
+    setTimeout(addAccorionClasses, 0, accordionOpenElem, accordionOpenElemParent)
+}
---
+++ js/catalog_artist.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.catalog__menu-item-item-artist').forEach(function(artistClick) {
+        artistClick.addEventListener('click', function(event) {
+            const artistClickElem = event.currentTarget
+            const artistClickParent = artistClickElem.parentNode
+            const artistClickNumber = artistClickParent.dataset.catalogItem
+            const artistClickArticle = document.querySelector(`[data-catalog-artist = "${artistClickNumber}"]`)
+
+            const artistEmptyInner = `
+            <button class="artist-card-item-picture">
+              <picture>
+                <source srcset="img/artist-x_320.png" media="(max-width: 320px)">
+                <source srcset="img/artist-x_768.png" media="(max-width: 768px)">
+                <source srcset="img/artist-x_1024.png" media="(max-width: 1024px)">
+                <source srcset="img/artist-x_1920.png" media="(min-width: 1025px)">
+                <img class="artist-card-item-img" src="img/artist-x_1920.png" alt="">
+              </picture>
+            </button>
+            <span class="artist-card-item-name">Что мы о нём знаем?</span>
+            <span class="artist-card-item-years"></span>
+            <p class="artist-card-item-description">
+              Пока ничего... Зато мы точно знаем, что в галерее есть на что посмотреть!
+            </p>
+            <a class="artist-card-item-link" href="#gallery">В галерею</a>
+            `
+
+            if (artistClickArticle.childNodes.length > 0) {
+                removeArtistOpen()
+                addArtist(artistClickArticle, artistClickElem)
+            } else {
+                artistClickArticle.innerHTML = artistEmptyInner
+                removeArtistOpen()
+                addArtist(artistClickArticle, artistClickElem)
+            }
+        })
+    })
+})
+
+function removeArtistOpen() {
+    document.querySelectorAll('.catalog__artist-card-item').forEach(function(classRemove0) {
+        classRemove0.classList.remove('catalog__artist-card-item-on')
+    })
+    document.querySelectorAll('.catalog__menu-item-item-artist').forEach(function(classRemove1) {
+        classRemove1.classList.remove('catalog__item-artist-on')
+    })
+}
+
+function addArtist(artistClickArticle, artistClickElem) {
+    artistClickArticle.classList.add('catalog__artist-card-item-on')
+    artistClickElem.classList.add('catalog__item-artist-on')
+}
