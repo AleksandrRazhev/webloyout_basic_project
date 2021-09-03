2619e4a1a7e81fe74f7e8c3edd9672efaf8989d9
RazhevAleksandr <alexandrrazh@gmail.com>
2021-09-03 19:31:21 +0300

    section publications finished

---
+++ js/publications.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const categories = document.querySelectorAll('.publications__form-categories-item')
+    const inputs = document.querySelectorAll('.publications__form-categories-checkbox')
+    const categoriesListBtn = document.querySelector('.publications__form-categories-article')
+    const categoriesList = document.querySelector('.publications__form-categories-block')
+    const categoriesButton = document.querySelector('.publications__form-categories-article')
+    
+    const showCloseBtn = () => {
+        inputs.forEach(item => {
+            if (item.checked) {
+                item.parentElement.classList.add('publications__form-categories-item-shown')
+            } else {
+                item.parentElement.classList.remove('publications__form-categories-item-shown')
+            }
+        })
+    }
+    
+    const listenClickCategorie = () => {
+        categories.forEach(item => {
+            item.addEventListener('click', event => {
+                showCloseBtn()
+            })
+        })
+    }
+
+    const closeCategorieShown = elem => {
+        elem.preventDefault()
+        elem.currentTarget.parentElement.querySelector('.publications__form-categories-checkbox').checked = false
+    }
+        
+    const listenClickCategorieShown = () => {
+        const categorieShown = document.querySelectorAll('.publications__form-categories-close-btn')
+        categorieShown.forEach(item => item.addEventListener('click', closeCategorieShown))
+    }
+
+    const closeList = (event) => {
+        event.preventDefault()
+        categoriesList.classList.toggle('publications__form-categories-block-close')
+        categoriesButton.classList.toggle('publications__form-categories-article-close')
+    }
+
+    const listenClosedList = () => categoriesListBtn.addEventListener('click', closeList)
+
+
+    showCloseBtn()
+    listenClickCategorie()
+    listenClickCategorieShown()
+    listenClosedList()
+})
